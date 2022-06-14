3e164e2cd6d4c1d619f5dccc9c66ba267423b397
pmessan <p.newman.messan@gmail.com>
2022-06-14 20:29:18 +0200

    feat(deployment): add cloundinary plugin and render for deployment

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('RENDER_EXTERNAL_HOSTNAME', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('0.0.0.0'),
+  port: env.int(1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
